254f10164c4fa9ccfc368f49b8d31bd5317493d4
quentin <quentin.sommesous@sfr.fr>
2020-02-21 00:01:25 +0100

    Traduction de tout les fichiers twig en francais + ajout des icones create.png édité.png

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin//user")
+ * @Route("/admin/user")
  */
 class AdminUserController extends AbstractController
 {
