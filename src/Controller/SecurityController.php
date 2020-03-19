6abfd4dce8014f23c71ad6ccbaade9510ccf827d
quentin <quentin.sommesous@sfr.fr>
2020-03-19 17:59:01 +0100

    mise en place de la parti public avec home editon profil edition de l'image update de la partie order coter utilisateur update des orders coté admin mise en place des news coté admin avec mise en place des image vich

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\AvatarRepository;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/signin", name="app_signin")
+     * @param AvatarRepository $repository
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return RedirectResponse|Response
+     * @throws Exception
+     */
+    public function add(AvatarRepository $repository, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $avatar = $repository->findOneBy(['id' => $_POST['imgProfil']]);
+            $em = $this->getDoctrine()->getManager();
+            $user->setAvatar($avatar);
+            $user->setStatus('visiteur');
+            $user->setRoles(['ROLE_USER']);
+            $user->setCreatedAt(new \DateTime());
+            $user->setPassword($passwordEncoder->encodePassword($user,$user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('security/signin.html.twig', [
+            'form' => $form->createView(),
+            'avatars' => $repository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     * @throws Exception
+     */
+    public function logout()
+    {
+        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+}
